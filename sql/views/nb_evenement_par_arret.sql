1f392722727175b68d26116cad0181008567b2f4
BenslimaneNassim <nassim.benslimane@etu.univ-amu.fr>
2025-05-07 00:55:49 +0200

    cleaning

--- sql/views/nb_evenement_par_arret.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE VIEW nb_evenements_par_arret AS
-SELECT arret_name, COUNT(*) AS nb_evenements
-FROM evenements_arrets
-GROUP BY arret_name
-ORDER BY nb_evenements DESC;
--- sql/views/restaurants_par_arret_nb.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE VIEW nb_restaurants_par_arret AS
-SELECT arret_name, COUNT(*) AS nb_restaurants
-FROM restaurant_nearest_stops
-GROUP BY arret_name
-ORDER BY nb_restaurants DESC;
